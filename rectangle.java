623d973f370178adb2aa5dfcee67c71bc2afec2b
chekkabhanu88 <138876647+chekkabhanu88@users.noreply.github.com>
2023-07-26 18:28:24 +0530

    rectangle

---
+++ rectangle.java
@@ -0,0 +1,19 @@
+package com.abc.graphics;
+
+
+public class rectangle
+{
+	 
+	 
+  private double length,breadth;
+  public rectangle(){}
+  public rectangle (double length,double breadth)
+  {
+  this.length=length;
+  this.breadth=breadth;
+  }
+  public double area()
+  {
+  return length * breadth;
+  }
+}
